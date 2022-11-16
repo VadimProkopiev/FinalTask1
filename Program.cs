d4f8a709fbee23dbbf36487332a17c6bae1635f2
«Vadim <Hazz74@yandex.ru>
2022-11-16 19:19:44 +0500

    Write cod program and add file FinalTaskScreenShot.jpg

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿string[] arr = new string[3] {"Russia", "Denmark", "Kazan"};
+string[] arr2 = new string[arr.Length];
+void SecondArrayWithIF(string[] arr, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+    if(arr[i].Length <= 3)
+        {
+        arr2[count] = arr[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(arr, arr2);
+PrintArray(arr2);
